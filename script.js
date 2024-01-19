6673c344604145c4560d99bd37871b2c56842803
perwriters <perwriters@gmail.com>
2024-01-19 15:15:08 +0300

    full screen view

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const testimonial = document.querySelector(".testimonial");
 const userImage = document.querySelector(".user-image");
 const username = document.querySelector(".username");
 const role = document.querySelector(".role");
+const numberELement = document.querySelector(".digit");
 
 const testimonials = [
   {
+    number: "1",
     name: "Miyah Myles",
     position: "Marketing",
     photo:
@@ -13,30 +15,35 @@ const testimonials = [
     text: "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
   },
   {
+    number: "2",
     name: "June Cha",
     position: "Software Engineer",
     photo: "https://randomuser.me/api/portraits/women/44.jpg",
     text: "This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!",
   },
   {
+    number: "3",
     name: "Iida Niskanen",
     position: "Data Entry",
     photo: "https://randomuser.me/api/portraits/women/68.jpg",
     text: "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
   },
   {
+    number: "4",
     name: "Renee Sims",
     position: "Receptionist",
     photo: "https://randomuser.me/api/portraits/women/65.jpg",
     text: "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
   },
   {
+    number: "5",
     name: "Jonathan Nunfiez",
     position: "Graphic Designer",
     photo: "https://randomuser.me/api/portraits/men/43.jpg",
     text: "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
   },
   {
+    number: "6",
     name: "Sasha Ho",
     position: "Accountant",
     photo:
@@ -44,6 +51,7 @@ const testimonials = [
     text: "This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!",
   },
   {
+    number: "7",
     name: "Veeti Seppanen",
     position: "Director",
     photo: "https://randomuser.me/api/portraits/men/97.jpg",
@@ -54,13 +62,14 @@ const testimonials = [
 let index = 1;
 
 const updateTestimonial = () => {
-  const { name, position, photo, text } = testimonials[index];
+  const { name, position, photo, text, number } = testimonials[index];
   testimonial.innerHTML = text;
   userImage.src = photo;
   username.innerHTML = name;
+  numberELement.innerHTML = number;
   role.innerHTML = position;
   index++;
   if (index > testimonials.length - 1) index = 0;
 };
 
-setInterval(updateTestimonial, 10000);
+setInterval(updateTestimonial, 5000);
